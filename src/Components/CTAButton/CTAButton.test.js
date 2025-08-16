0d82152efded5c7c00d9b018df4e570f59a7ba05
kainenw <108428180+kainenw@users.noreply.github.com>
2025-08-15 19:16:36 -0500

    Ensure client components for CTAButton imports

--- src/Components/CTAButton/CTAButton.test.js
+++ src/Components/CTAButton/CTAButton.test.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { render, screen, fireEvent } from '@testing-library/react';
 import CTAButton from './CTAButton';
 
--- src/Components/HeaderCTA.js
+++ src/Components/HeaderCTA.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react';
 import CTAButton from './CTAButton/CTAButton';
 
